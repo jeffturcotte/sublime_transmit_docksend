41ae3271fa9fca6025e22b9e071e32af55e90efa
Jeff Turcotte <jeff.turcotte@gmail.com>
2011-08-11 14:04:58 -0400

    initial commit

---
+++ TransmitDocksend.py
@@ -0,0 +1,22 @@
+import sublime
+import sublime_plugin
+import subprocess
+
+class TransmitDocsendCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		script = """
+		on run
+			ignoring application responses
+				tell application "Transmit"
+					open POSIX file "%s"
+				end tell
+			end ignoring
+		end run
+		"""
+
+		proc = subprocess.Popen(
+			["osascript", "-e", script % self.view.file_name()], 
+			stdin=subprocess.PIPE,
+			stdout=subprocess.PIPE, 
+			stderr=subprocess.STDOUT
+		)
