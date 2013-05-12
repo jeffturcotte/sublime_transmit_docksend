9c62268e11e073c16ce403b8e927aa530deed487
mfallend <mfallend@gmail.com>
2013-05-12 15:51:34 +0300

    add "Send with Active Connection"

--- TransmitDocksend.py
+++ TransmitDocksend.py
@@ -3,16 +3,29 @@ import sublime_plugin
 import subprocess
 
 class TransmitDocksendCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		script = """
-		on run
-			ignoring application responses
-				tell application "Transmit"
-					open POSIX file "%s"
+	def run(self, edit, connection=False):
+		if connection == 'active':
+			script = """
+			on run
+			tell application "Transmit"
+				tell current tab of front document
+					tell remote browser
+						upload item at path "%s"
+					end tell
 				end tell
-			end ignoring
-		end run
-		"""
+			end tell
+			end run
+			"""
+		else:
+			script = """
+			on run
+				ignoring application responses
+					tell application "Transmit"
+						open POSIX file "%s"
+					end tell
+				end ignoring
+			end run
+			"""
 
 		proc = subprocess.Popen(
 			["osascript", "-e", script % self.view.file_name()], 
